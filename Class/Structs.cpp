916b57179ccc07e2057a209116abd82b4fcb65be
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2024-01-05 18:39:42 +0200

    Create Structs.cpp

---
+++ Class/Structs.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+    add code for struct here.
+*/
+struct Student
+{
+    int age, standard;
+    string first_name, last_name;
+};
+
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
+
+// Problem Link: https://www.hackerrank.com/challenges/c-tutorial-struct/problem?isFullScreen=true
